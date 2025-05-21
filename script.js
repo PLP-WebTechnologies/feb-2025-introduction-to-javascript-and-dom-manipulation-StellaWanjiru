1a5a4fa77b0fcceb77aa9237696acfa8b366121c
StellaWanjiru <stellawanjiru42@gmail.com>
2025-05-21 09:15:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-/* This script dynamically changes textContent when the button is clicked */
-const button =document.getElementById("orderButton");
-const message=document.getElementById("ordermessage");
-
-button.addEventListener("Click",()=> {
-    message.textContent="Your order has been placed!";
-});
 
 /* This script Modifies CSS styles via JavaScript. */
 const text=document.getElementById("welcomeMessage");
 text.style.color="Purple";
 text.style.fontsize="30px"
 
+/* This script dynamically changes textContent when the order now button is clicked */
+const button =document.getElementById("btn");
+const message=document.getElementById("message");
+button.addEventListener("click",()=> {
+    message.textContent="Your placed an order! ";
+});
+
 /* This script Adds  an element when a button is clicked. */
 const joinButton=document.getElementById("joinBtn");
 const list=document.getElementById("list");
 joinButton.addEventListener("click",()=>{
-const newItem=document.createElement("li");
-newItem.textContent="New Member";
-list.appendChild(newItem);
+    const newItem=document.createElement("li");
+    newItem.textContent="New Member";
+    list.appendChild(newItem);
+});
+
